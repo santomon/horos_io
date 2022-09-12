7c667b740412b6b1a8ca8ceab50117425d2ee6d7
Quang <qa12_8@yahoo.de>
2022-09-12 21:49:16 +0200

    cli prototype; not fully tested yet

--- horos_io/cli.py
+++ horos_io/cli.py
@@ -1,10 +1,30 @@
-"""Console script for horos_io."""
+"""
+Console script for horos io;
+will incorporate functionality for running visual sanity checks on contours overlayed on top of the data;
+
+generating .csv files for data
+"""
+import os
+import pathlib
+import re
 import sys
+import typing
+from typing import Optional, NoReturn
 
 import click
+import pandas as pd
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+from horos_io import config
+from horos_io._utils import globSSF, mask_from_omega_contour, combined_info, get_log, get_log_path
+from horos_io.horos_io import Path, _get_slice_type, _load_basaL_first_as_list, _get_n_frames_from_seq_path, \
+    _get_n_slices_from_seq_path
+from horos_io.ui import shitty_manual_confirm
+from horos_io.validation import visually_confirm_omega_iter, write_log, last_validation_was_successful
 
 
-@click.command()
+@click.group()
 def main():
     """Console script for horos_io."""
     click.echo("Replace this message by putting your code into "
@@ -12,6 +32,219 @@ def main():
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
+
+@click.command("image_info",
+               help="saves information about the horos dataset as csv, such that it can be later used for efficient"
+                    "loading of the data")
+@click.option("--root", default=".", help="directory to the Horos Dataset")
+@click.option("--out", default=None,
+              help="how the image_info,csv should be saved; if not specified, will save an image_info.csv in root")
+def make_image_info_csv(root: Path, basal_info_file: Optional[str] = None,
+                        out: Optional[Path] = None) -> NoReturn:
+    """
+    one-time operation to update the information of the dataset;
+
+    basal_info_file is but a csv document, that contains, if the sax stack has basal slice first
+    in the sequence. is needed for having consistent ordering across sequences
+
+    columns:
+    ("seq_path",  <relative path starting from the root of the data>
+     "location",  joins the rootdir with seq path
+     "ID",        <Impression_Cmr{ID}>
+     "slice_type", one of config.slice_types
+     "n_frames",
+     "n_slices",
+     "basal_first", one of NA, True or False; only relevant for loading SAX in correct order)
+
+    seq_path is relative to the root
+    location is path relative to the source_root (ventricular-function/container/files)
+
+    creates a csv file containing location of the sequence.
+    sequence can be loaded with load_sequence
+    Args:
+        root: root of the data; contents should be directories of Impression Studies
+        out: optional parameter to specify, what and where to save
+    Returns:
+    """
+
+    if basal_info_file is None:
+        basal_info_file = os.path.join(root, "basal_info.csv")
+
+    basal_firsts = _load_basaL_first_as_list(basal_info_file)
+    result = pd.DataFrame()
+
+    result["seq_path"] = globSSF("*/*/*/", root_dir=root)
+    result["ID"] = result["seq_path"].apply(lambda loc: pathlib.Path(loc).parts[0])  # ID is name of the first folder
+    result["location"] = [os.path.normpath(os.path.join(root, seq_path))
+                          for seq_path in globSSF("*/*/*/", root_dir=root)]
+    result["slice_type"] = result["location"].apply(_get_slice_type)
+    result["n_frames"] = result["location"].apply(_get_n_frames_from_seq_path)
+    result["n_slices"] = result["location"].apply(_get_n_slices_from_seq_path)
+
+    # true if sax and contained in basal_first
+    # NaN if lax
+    result["basal_first"] = result.apply(lambda row: re.findall(r"\d{4}", row["ID"])[0] in basal_firsts
+    if row["slice_type"] == "cine_sa" else pd.NA, axis=1)
+
+    if out is None:
+        out = os.path.normpath(os.path.join(root, "image_info.csv"))
+    result.sort_values(by="ID").to_csv(out)
+
+
+@click.command("contour_info",
+               help="saves information about the horos dataset as csv, such that it can be later used for efficient"
+                    "loading of the data")
+@click.option("--root", default=".", help="directory to the Horos Dataset")
+@click.option("--out", default=None,
+              help="how the image_info,csv should be saved; if not specified, will save an image_info.csv in root")
+def make_contour_info_csv(root: Path, out: Optional[Path] = None) -> NoReturn:
+    """
+    one-time operation to update the information of the dataset;
+
+    ("ID": <Impression_Cmr{ID}>,
+     "contour_path": relative path to the contour file, starting from root of the data,
+     "contour_type": one of config.contour_types,
+     "location": joins the root dir with the sequence path
+     "slice_type": one of config.slice_types,
+     "c
+    )
+
+    creates a csv file containing location of all contour files; or whether they exist at all.
+    Args:
+        root: root of the data; contents should be directories of Impression Studies
+    Returns:
+    """
+    root = os.path.normpath(root)
+    click.echo(f"creating contour info for {root}")
+
+    result = pd.concat([_make_contour_info_csv_by_contour_type(root, contour_type)
+                        for contour_type in tqdm(config.contour_types)], ignore_index=True)
+
+    result["location"] = result["contour_path"].apply(
+        lambda path: os.path.normpath(os.path.join(root, path)) if pd.notna(path) else ""
+    )
+    result["location"].replace("", pd.NA, inplace=True)
+    if out is None:
+        out = os.path.normpath(os.path.join(root, "contour_info.csv"))
+    click.echo(f"saving contour info to: {out}")
+    result.sort_values(by="ID").to_csv(out)
+
+
+def _make_contour_info_csv_by_contour_type(root: Path, contour_type: str) -> pd.DataFrame:
+    result = pd.DataFrame()
+    result["ID"] = globSSF(f"Impression*", root_dir=root)
+    result["contour_path"] = result["ID"].apply(
+        lambda id_: os.path.join(id_, f"{contour_type}.xml")
+        if os.path.isfile(os.path.join(root, id_, f"{contour_type}.xml"))
+        else ""
+    )
+    result["slice_type"] = result["contour_path"].apply(lambda x: _get_slice_type(contour_type))
+
+    result["contour_path"].replace("", pd.NA, inplace=True)
+    result["contour_type"] = contour_type
+    return result
+
+
+@click.command()
+@click.option("--by", required=True, help="the name that will be recorded in the log_")
+@click.option("--root", default=".", help="source root of the Horos dataset")
+@click.option("--only_unconfirmed", is_flag=True, default=False,
+              help="will only prompt you cases, that have not been confirmed by the user yet (registered in log_)")
+@click.option("--log", default=None,
+              help="if None, will save or update a val_contour_log.csv in root, else checks your specified log_ instead")
+def validate(by: str, root: Path, only_unconfirmed: bool, log: Path):
+    """
+    visually confirm literally every existing omega contour... (might need to change with params to return
+    an adequate iterator)
+
+    will also save a dataframe / update a dataframe in <root>/val_contour_log.csv,
+    that logs review result with timestamp
+    """
+    click.echo(f"starting validation process in root: {root}")
+
+    for ID, omega, f, s, cines, contours in tqdm(visually_confirm_omega_iter(combined_info=combined_info(root))):
+        if only_unconfirmed and last_validation_was_successful(get_log(log, root), ID=ID, frame=f, slice=s,
+                                                               contour_type=omega):  # CAVE: hard coded
+            continue
+        fig, ax = plt.subplots()
+        ax.imshow(cines[f, s].pixel_array)
+        ax.imshow(mask_from_omega_contour(cines, contours, f, s), cmap="jet", alpha=0.5)
+        ax.figure.suptitle(f"{ID}: {omega} f: {f}, s: {s}, ")
+        ok, remark = shitty_manual_confirm(ax)
+        write_log(log, **locals())
+    click.echo("batch finished validation!")
+
+
+# not sure if we should cli this
+def _rename_contour_files(root: str, rename_dict: typing.Optional[dict]) -> typing.NoReturn:
+    """
+    onetime io operation to fix namings of xml files
+
+    renames .xml and .roi_series files by dictionary, that lie in the impression folders:
+    root |--- Impression_CmrXXXX
+                    |---- lv_epi.xml
+                    |---- lv_epi.roi_series
+    Args:
+        root:
+        rename_dict:
+
+    Returns:
+    """
+    files = globSSF("Impression*/*.xml", root_dir=root) + globSSF("Impression*/*.rois_series", root_dir=root)
+    for f in files:
+        path, f_name = os.path.split(f)
+        f_name, f_ending = f_name.split(".")
+        if f_name in rename_dict.keys():
+            src = os.path.normpath(os.path.join(root, path, f"{f_name}.{f_ending}"))
+
+            dst = os.path.normpath(os.path.join(root, path, f"{rename_dict[f_name]}.{f_ending}"))
+            if os.path.isfile(dst):
+                raise FileExistsError(f"{dst} for {src} already exists")
+            os.rename(src, dst)
+
+@click.group("check", help="given a root and a log file, checks if the user (--by) has seen all the samples")
+def check():
+    pass
+
+@click.option("--by", required=True, help="the name to check for in the logs")
+@click.option("--root", default=".", help="source root to the Horos dataset")
+@click.option("--log", default=None,
+              help="if None, will check for val_contour_log.csv in root, else checks your specified log instead")
+@click.command("failed")
+def check_failed(by: str, root: str, log: str):
+    click.echo(f"checking for failed validations by {by} for dataset in {root}, using logfile {get_log_path(log, root)}")
+    log_df = get_log(log, root)
+    failed = log_df.apply(lambda row: not last_validation_was_successful(log_df,
+                                                                by=row["by"],
+                                                                ID=row["ID"],
+                                                                frame=row["frame"],
+                                                                slice=row["slice"]), axis=1)
+    result = log_df[(log_df["by"] == by) & failed]
+    result = result[["by", "ID", "frame", "slice"]].drop_duplicates()
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        click.echo(result)
+        click.echo("----------------")
+        click.echo(f"These samples did not end with confirmation by user {by}")
+
+
+
+
+@click.command("unobserved")
+@click.option("--root", default=".", help="source root to the Horos dataset")
+@click.option("--log", default=None,
+              help="if None, will check for val_contour_log.csv in root, else checks your specified log instead")
+@click.option("--by", required=True, help="the name to check for in the logs")
+def check_unobserved(by: str, root: str, log: str):
+    pass
+
+
+
+main.add_command(make_image_info_csv)
+main.add_command(make_contour_info_csv)
+main.add_command(validate)
+main.add_command(check)
+check.add_command(check_failed)
+check.add_command(check_unobserved)
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover

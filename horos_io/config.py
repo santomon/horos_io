fbf0826209e9b3a63962e5d7c2791415cc17da8e
Quang <qa12_8@yahoo.de>
2022-09-12 14:12:30 +0200

    config file for information slice types; etc.

---
+++ horos_io/config.py
@@ -0,0 +1,25 @@
+
+slice_types = ("cine_sa", "cine_2ch", "cine_3ch", "cine_4ch")
+
+contour_types = (
+    "sax_lv_endo",
+    "sax_lv_epi",
+    "sax_rv",
+    "lax_2ch_lv_wall",
+    "lax_2ch_lv_vol",
+    "lax_2ch_la",
+    "lax_3ch_lv_wall",
+    "lax_3ch_lv_vol",
+    "lax_3ch_rv",
+    "lax_4ch_lv_wall",
+    "lax_4ch_lv_vol",
+    "lax_4ch_rv",
+    "lax_4ch_la",
+    "lax_4ch_ra",
+    "omega_4ch"
+)
+
+omega_types = ("omega_4ch", "omega_3ch", "omega_2ch", "omega_sax")
+
+#TODO: omega_3ch_names, omega_2ch_names, omega_sax_names
+omega_4ch_names = ("rv_vol", "lv_vol", "ra", "la", "lv_wall")  # now also denotes plotting order

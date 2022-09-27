c23b25423ef0eaf72e211b219293b70a1712f04f
santomon <qa12_8@yahoo.de>
2022-09-27 23:21:59 +0200

    mask_from_omega_contour  now also part of main line up

--- horos_io/__init__.py
+++ horos_io/__init__.py
@@ -9,3 +9,4 @@ from .cmr import load_lax_sequence, sort_SAX_by_y, load_cine_sequence, load_sax_
 
 from .core import load_horos_contour, get_n_frames_from_seq_path, get_n_slices_from_seq_path, \
     load_horos_contour_unstructured
+from ._utils import mask_from_omega_contour
